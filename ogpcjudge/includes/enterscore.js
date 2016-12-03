97b9dff6e76845f212951390e801d6908e85b7b9
rogersad <alandrogers@gmail.com>
2016-12-03 09:34:21 -0800

    add warning if team + category already scored

--- ogpcjudge/includes/enterscore.js
+++ ogpcjudge/includes/enterscore.js
@@ -1,23 +1,6 @@
 
-//locks team name selection:
-/* no longer used
-function lock(){
-	if ($('#locked').val() == 'true'){
-		if (confirm('Unlock team list?')){
-			$('#locked').val('false');
-			$('#lockTeam').val('Lock').removeClass('locked').addClass('notlocked');
-			$('#teamID :not(:selected)').attr('disabled',false);
-		}
-	}
-	else {//lock the team list:
-		$('#locked').val('true');
-		$('#lockTeam').val('Locked').removeClass('notlocked').addClass('locked');
-		$('#teamID :not(:selected)').attr('disabled','disabled');
-	}
-}
-*/
 
-// moved ajax load to select.change
+// moved ajax load to select.change only necessary first time.
 $( "#categoryID" ).change(function() {
   loadAchList($('#categoryID option:selected').val());
 });
@@ -26,6 +9,50 @@ $( "#categoryID" ).change(function() {
 $('#btnSubmit').click(function(){
 	confirmscore();
 });
+
+//add warning if team + category already scored (change func for both teamID,categoryID):
+/* */
+$("#teamID").change(function() {
+	var teamId = $("#teamID option:selected").val();
+	var catId = $("#categoryID option:selected").val();
+	
+	if(teamId > 0 && catId > 0) {
+		$.post("index.cfm?action=ajax.getScoresJSON",{teamID: teamId, categoryID:catId}).done(function(rdata) {
+			showScoringError(rdata);
+		});
+	}
+});
+
+$("#categoryID").change(function() {
+	var teamId = $("#teamID option:selected").val();
+	var catId = $("#categoryID option:selected").val();
+	
+	if(teamId > 0 && catId > 0) {
+		$.post("index.cfm?action=ajax.getScoresJSON",{teamID: teamId, categoryID:catId}).done(function(rdata) {
+			showScoringError(rdata);
+		});
+	}
+});
+
+//DATA will be number of rows in achievements table for selected team + category
+function showScoringError(rdata){
+	var rowCount = rdata.DATA;  
+
+	if(rowCount > 0){
+		var warningText = 'WARNING! This team has already been scored for this category.<br />';
+		/*
+		warningText += '<div id="update" class="form-group"><label class="inline">';
+		warningText += '<input id="reloadPrior" type="button" class="btn btn-default"';
+		warningText += 'onClick="copyTC();submitUpdate();" value="Update Existing Score" /></label></div>';
+		*/	
+		$("#warning").html(warningText);
+		$("#warning").addClass('show');
+	}
+	else { // all good, hide.
+		$("#warning").removeClass('show');
+	}
+}
+
 
 // AJAX load of achievemetns when getQ button is clicked.
 function loadAchList(achId){
